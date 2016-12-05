83263cdd54622f5b148df04c5ad166b36d97447a
Philip Young <philip.young.gunawan@gmail.com>
2016-12-05 21:23:59 +0700

    refactor code to OOP

--- slideTransform.js
+++ slideTransform.js
@@ -1,16 +1,66 @@
 'use strict';
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-// //////////////
-// TO USE: assign variable to an element
-// var slide = slideTransform({element: 'slide-container'})
-// /////////////
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 (function (window) {
   function SlideTransform(config) {
+    var _this = this;
+
+    var initializeSlidesContainer = function initializeSlidesContainer() {
+      _extends(_this.container.style, {
+        position: 'relative',
+        overflow: 'hidden'
+      });
+
+      var proxySlidesContainer = document.createElement('div');
+      proxySlidesContainer.classList.add('slides-container');
+      _extends(proxySlidesContainer.style, {
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        transition: '0.3s ease-in-out',
+        webkitTransition: '0.3s ease-in-out'
+      });
+
+      // cut all slides from container to slidesContainer
+      while (_this.container.children.length > 0) {
+        proxySlidesContainer.appendChild(_this.container.children[0]);
+      }
+
+      [].forEach.call(proxySlidesContainer.children, function (slide, index) {
+        _extends(slide.style, {
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+          transform: 'translateX(' + index * 100 + '%)',
+          webkitTransform: 'translateX(' + index * 100 + '%)'
+        });
+      });
+      _this.container.appendChild(proxySlidesContainer);
+    };
+
+    var renderStepsContainer = function renderStepsContainer() {
+      var stepsContainer = document.createElement('div');
+      stepsContainer.classList.add('steps-container');
+      _this.container.appendChild(stepsContainer);
+
+      for (var index = 0; index < _this.container.children[0].children.length; index++) {
+        var step = document.createElement('div');
+        step.classList.add('step');
+        step.setAttribute('slide-index', index);
+        step.addEventListener('click', function (e) {
+          _this.goToSlide(Number(e.target.getAttribute('slide-index')));
+        });
+        stepsContainer.appendChild(step);
+      }
+
+      stepsContainer.children[_this.currIndex].classList.add('active');
+    };
+
+    // initialize
+
     if ((typeof config === 'undefined' ? 'undefined' : _typeof(config)) !== 'object') {
       throw "you didn't provide object. Refer to documentation.";
     }
@@ -20,54 +70,24 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
     this.navigation = typeof config.navigation === 'undefined' ? true : config.navigation;
     this.container = document.querySelector(config.element);
+    if (!this.container) throw 'container element not found';
     this.totalIndex = this.container.children.length;
     this.currIndex = config.index || 0;
     this.prevIndex = this.currIndex;
 
-    this.initializeSlidesContainer();
+    initializeSlidesContainer();
 
     if (this.navigation) {
-      this.renderCirclesContainer();
-      this.circlesContainer = this.container.querySelector('.circles-container');
+      renderStepsContainer();
+      this.stepsContainer = this.container.querySelector('.steps-container');
     }
 
     this.goToSlide(this.currIndex);
+
+    window.SlideTransform = SlideTransform; //eslint-disable-line
   }
 
-  SlideTransform.prototype.initializeSlidesContainer = function () {
-    _extends(this.container.style, {
-      position: 'relative',
-      overflow: 'hidden'
-    });
-
-    var proxySlidesContainer = document.createElement('div');
-    proxySlidesContainer.classList.add('slides-container');
-    _extends(proxySlidesContainer.style, {
-      width: '100%',
-      height: '100%',
-      position: 'absolute',
-      transition: '0.3s ease-in-out'
-    });
-
-    // cut all slides from container to slidesContainer
-    while (this.container.children.length > 0) {
-      proxySlidesContainer.appendChild(this.container.children[0]);
-    }
-
-    [].forEach.call(proxySlidesContainer.children, function (slide, index) {
-      _extends(slide.style, {
-        width: '100%',
-        height: '100%',
-        position: 'absolute',
-        transform: 'translateX(' + index * 100 + '%)',
-        webkitTransform: 'translateX(' + index * 100 + '%)',
-        MozTransform: 'translateX(' + index * 100 + '%)'
-      });
-    });
-    this.container.appendChild(proxySlidesContainer);
-  };
-
-  SlideTransform.prototype.goToSlide = function (newIndex) {
+  SlideTransform.prototype.goToSlide = function goToSlide(newIndex) {
     if (newIndex >= 0 && newIndex < this.totalIndex) {
       this.prevIndex = this.currIndex;
       this.currIndex = newIndex;
@@ -75,76 +95,46 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
       _extends(this.container.children[0].style, {
         transform: 'translateX(' + offset + '%)',
-        webkitTransform: 'translateX(' + offset + '%)',
-        MozTransform: 'translateX(' + offset + '%)'
+        webkitTransform: 'translateX(' + offset + '%)'
       });
 
       if (this.navigation) {
-        this.container.children[1].children[this.prevIndex].classList.remove('active');
-        this.container.children[1].children[this.currIndex].classList.add('active');
+        var slidesContainer = this.container.querySelector('.slides-container');
+        slidesContainer.children[this.prevIndex].classList.remove('active');
+        slidesContainer.children[this.currIndex].classList.add('active');
       }
     } else {
       console.error('slide index error, report this when you encountered this error.'); //eslint-disable-line
     }
   };
 
-  SlideTransform.prototype.nextSlide = function () {
+  SlideTransform.prototype.nextSlide = function nextSlide() {
     if (this.currIndex < this.totalIndex - 1) {
       var newIndex = this.currIndex + 1;
       this.goToSlide(newIndex);
     }
   };
 
-  SlideTransform.prototype.prevSlide = function () {
+  SlideTransform.prototype.prevSlide = function prevSlide() {
     if (this.currIndex > 0) {
       var newIndex = this.currIndex - 1;
       this.goToSlide(newIndex);
     }
   };
 
-  SlideTransform.prototype.subscribeNextSlide = function () {
-    var _this = this;
-
-    document.querySelector().addEventListener('click', function () {
-      _this.nextSlide();
-    });
-  };
-
-  SlideTransform.prototype.subscribePrevSlide = function () {
+  SlideTransform.prototype.subscribeNextSlide = function subscribeNextSlide() {
     var _this2 = this;
 
     document.querySelector().addEventListener('click', function () {
-      _this2.prevSlide();
+      _this2.nextSlide();
     });
   };
 
-  SlideTransform.prototype.renderCirclesContainer = function () {
+  SlideTransform.prototype.subscribePrevSlide = function subscribePrevSlide() {
     var _this3 = this;
 
-    var circlesContainer = document.createElement('div');
-    circlesContainer.classList.add('circles-container');
-    _extends(circlesContainer.style, {
-      position: 'absolute',
-      bottom: 0,
-      width: '100%'
+    document.querySelector().addEventListener('click', function () {
+      _this3.prevSlide();
     });
-    this.container.appendChild(circlesContainer);
-
-    for (var index = 0; index < this.container.children[0].children.length; index++) {
-      var circle = document.createElement('div');
-      circle.classList.add('circle');
-      circle.setAttribute('slide-index', index);
-      circle.addEventListener('click', function (e) {
-        _this3.goToSlide(Number(e.target.getAttribute('slide-index')));
-      });
-      circlesContainer.appendChild(circle);
-    }
-
-    circlesContainer.children[this.currIndex].classList.add('active');
   };
-  window.SlideTransform = SlideTransform;
 })(window);
-
-var x = new SlideTransform({
-  element: '.container'
-});
--- src/src.js
+++ src/src.js
@@ -1,10 +1,58 @@
-// //////////////
-// TO USE: assign variable to an element
-// var slide = slideTransform({element: 'slide-container'})
-// /////////////
-
 ((window) => {
   function SlideTransform(config) {
+    const initializeSlidesContainer = () => {
+      Object.assign(this.container.style, {
+        position: 'relative',
+        overflow: 'hidden',
+      });
+
+      const proxySlidesContainer = document.createElement('div');
+      proxySlidesContainer.classList.add('slides-container');
+      Object.assign(proxySlidesContainer.style, {
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        transition: '0.3s ease-in-out',
+        webkitTransition: '0.3s ease-in-out',
+      });
+
+        // cut all slides from container to slidesContainer
+      while (this.container.children.length > 0) {
+        proxySlidesContainer.appendChild(this.container.children[0]);
+      }
+
+      [].forEach.call(proxySlidesContainer.children, (slide, index) => {
+        Object.assign(slide.style, {
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+          transform: `translateX(${index * 100}%)`,
+          webkitTransform: `translateX(${index * 100}%)`,
+        });
+      });
+      this.container.appendChild(proxySlidesContainer);
+    };
+
+    const renderStepsContainer = () => {
+      const stepsContainer = document.createElement('div');
+      stepsContainer.classList.add('steps-container');
+      this.container.appendChild(stepsContainer);
+
+      for (let index = 0; index < this.container.children[0].children.length; index++) {
+        const step = document.createElement('div');
+        step.classList.add('step');
+        step.setAttribute('slide-index', index);
+        step.addEventListener('click', (e) => {
+          this.goToSlide(Number(e.target.getAttribute('slide-index')));
+        });
+        stepsContainer.appendChild(step);
+      }
+
+      stepsContainer.children[this.currIndex].classList.add('active');
+    };
+
+    // initialize
+
     if (typeof config !== 'object') {
       throw "you didn't provide object. Refer to documentation.";
     }
@@ -14,54 +62,24 @@
 
     this.navigation = typeof config.navigation === 'undefined' ? true : config.navigation;
     this.container = document.querySelector(config.element);
+    if (!this.container) throw 'container element not found';
     this.totalIndex = this.container.children.length;
     this.currIndex = config.index || 0;
     this.prevIndex = this.currIndex;
 
-    this.initializeSlidesContainer();
+    initializeSlidesContainer();
 
     if (this.navigation) {
-      this.renderCirclesContainer();
-      this.circlesContainer = this.container.querySelector('.circles-container');
+      renderStepsContainer();
+      this.stepsContainer = this.container.querySelector('.steps-container');
     }
 
     this.goToSlide(this.currIndex);
+
+    window.SlideTransform = SlideTransform; //eslint-disable-line
   }
 
-  SlideTransform.prototype.initializeSlidesContainer = function () {
-    Object.assign(this.container.style, {
-      position: 'relative',
-      overflow: 'hidden',
-    });
-
-    const proxySlidesContainer = document.createElement('div');
-    proxySlidesContainer.classList.add('slides-container');
-    Object.assign(proxySlidesContainer.style, {
-      width: '100%',
-      height: '100%',
-      position: 'absolute',
-      transition: '0.3s ease-in-out',
-    });
-
-      // cut all slides from container to slidesContainer
-    while (this.container.children.length > 0) {
-      proxySlidesContainer.appendChild(this.container.children[0]);
-    }
-
-    [].forEach.call(proxySlidesContainer.children, (slide, index) => {
-      Object.assign(slide.style, {
-        width: '100%',
-        height: '100%',
-        position: 'absolute',
-        transform: `translateX(${index * 100}%)`,
-        webkitTransform: `translateX(${index * 100}%)`,
-        MozTransform: `translateX(${index * 100}%)`,
-      });
-    });
-    this.container.appendChild(proxySlidesContainer);
-  };
-
-  SlideTransform.prototype.goToSlide = function (newIndex) {
+  SlideTransform.prototype.goToSlide = function goToSlide(newIndex) {
     if (newIndex >= 0 && newIndex < this.totalIndex) {
       this.prevIndex = this.currIndex;
       this.currIndex = newIndex;
@@ -70,69 +88,41 @@
       Object.assign(this.container.children[0].style, {
         transform: `translateX(${offset}%)`,
         webkitTransform: `translateX(${offset}%)`,
-        MozTransform: `translateX(${offset}%)`,
       });
 
       if (this.navigation) {
-        this.container.children[1].children[this.prevIndex].classList.remove('active');
-        this.container.children[1].children[this.currIndex].classList.add('active');
+        const slidesContainer = this.container.querySelector('.slides-container');
+        slidesContainer.children[this.prevIndex].classList.remove('active');
+        slidesContainer.children[this.currIndex].classList.add('active');
       }
     } else {
         console.error('slide index error, report this when you encountered this error.'); //eslint-disable-line
     }
   };
 
-  SlideTransform.prototype.nextSlide = function () {
+  SlideTransform.prototype.nextSlide = function nextSlide() {
     if (this.currIndex < this.totalIndex - 1) {
       const newIndex = this.currIndex + 1;
       this.goToSlide(newIndex);
     }
   };
 
-  SlideTransform.prototype.prevSlide = function () {
+  SlideTransform.prototype.prevSlide = function prevSlide() {
     if (this.currIndex > 0) {
       const newIndex = this.currIndex - 1;
       this.goToSlide(newIndex);
     }
   };
 
-  SlideTransform.prototype.subscribeNextSlide = function () {
+  SlideTransform.prototype.subscribeNextSlide = function subscribeNextSlide() {
     document.querySelector().addEventListener('click', () => {
       this.nextSlide();
     });
   };
 
-  SlideTransform.prototype.subscribePrevSlide = function () {
+  SlideTransform.prototype.subscribePrevSlide = function subscribePrevSlide() {
     document.querySelector().addEventListener('click', () => {
       this.prevSlide();
     });
   };
-
-  SlideTransform.prototype.renderCirclesContainer = function () {
-    const circlesContainer = document.createElement('div');
-    circlesContainer.classList.add('circles-container');
-    Object.assign(circlesContainer.style, {
-      position: 'absolute',
-      bottom: 0,
-      width: '100%',
-    });
-    this.container.appendChild(circlesContainer);
-
-    for (let index = 0; index < this.container.children[0].children.length; index++) {
-      const circle = document.createElement('div');
-      circle.classList.add('circle');
-      circle.setAttribute('slide-index', index);
-      circle.addEventListener('click', (e) => {
-        this.goToSlide(Number(e.target.getAttribute('slide-index')));
-      });
-      circlesContainer.appendChild(circle);
-    }
-
-    circlesContainer.children[this.currIndex].classList.add('active');
-  };
-  window.SlideTransform = SlideTransform;
 })(window);
-
-const x = new SlideTransform({
-  element: '.container',
-});
